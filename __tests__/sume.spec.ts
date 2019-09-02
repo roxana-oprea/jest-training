689c0f1ab5e2fe4d7d0b9b882e8335656757bd98
roxana-oprea <oprearoxanaadolfina@gmail.com>
2019-09-02 16:19:09 +0300

    add tests ex 531

--- __tests__/sume.spec.ts
+++ __tests__/sume.spec.ts
@@ -6,6 +6,13 @@ import {
   addsPerfectSquaresNumbers,
   addsNumbersAtEvenPosition,
   addsNumbersDivisibleBy3,
+  addsNumbersDividedBy5,
+  sumOfSquares,
+  addsNumbersInInterval,
+  addsPositiveNumbersAndDevidedBy5,
+  addsNumbersDevidedBy3And2,
+  addsOddNumberAndHigherThan15,
+  addPerfectSquaresAndEvenNumbers,
 } from '../src/sume';
 
 describe('Sume', () => {
@@ -42,5 +49,40 @@ describe('Sume', () => {
   it('adds numbers which are divisible by 3', () => {
     const arr = Object.freeze([3, 1, 7, 9]) as number[];
     expect(addsNumbersDivisibleBy3(arr)).toBe(12);
+  });
+
+  it('adds numbers divided by 5 the rest is 3', () => {
+    const arr = Object.freeze([13, 8, 5, 2]) as number[];
+    expect(addsNumbersDividedBy5(arr)).toBe(21);
+  });
+
+  it('return the sum of squares', () => {
+    const arr = Object.freeze([2, 3, 4]) as number[];
+    expect(sumOfSquares(arr)).toBe(29);
+  });
+
+  it('adds numbers between interval [-1, 9]', () => {
+    const arr = Object.freeze([2, 12, -1, 8, 16]) as number[];
+    expect(addsNumbersInInterval(arr)).toBe(9);
+  });
+
+  it('adds positive numbers and divided by 5', () => {
+    const arr = Object.freeze([-2, 1, 5, -5, 10]) as number[];
+    expect(addsPositiveNumbersAndDevidedBy5(arr)).toBe(15);
+  });
+
+  it('adds numbers divided by 3 and 2 and are at even position', () => {
+    const arr = Object.freeze([2, 3, 6, 5, 12]) as number[];
+    expect(addsNumbersDevidedBy3And2(arr)).toBe(18);
+  });
+
+  it('adds odd numbers and higher than 15', () => {
+    const arr = Object.freeze([2, 3, 17, 19, 14]) as number[];
+    expect(addsOddNumberAndHigherThan15(arr)).toBe(36);
+  });
+
+  it('perfect squares numbers and are even', () => {
+    const arr = Object.freeze([4, 9, 19, 5, 6]) as number[];
+    expect(addPerfectSquaresAndEvenNumbers(arr)).toBe(4);
   });
 });
--- src/sume.ts
+++ src/sume.ts
@@ -56,3 +56,62 @@ export function addsNumbersDivisibleBy3(arr: number[]): number {
   }
   return sum;
 }
+
+export function addsNumbersDividedBy5(arr: number[]): number {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 5 === 3) sum += arr[i];
+  }
+  return sum;
+}
+
+export function sumOfSquares(arr: number[]): number {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i] * arr[i];
+  }
+  return sum;
+}
+
+export function addsNumbersInInterval(arr: number[]): number {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= -1 && arr[i] <= 9) sum += arr[i];
+  }
+  return sum;
+}
+
+export function addsPositiveNumbersAndDevidedBy5(arr: number[]): number {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0 && arr[i] % 5 === 0) sum += arr[i];
+  }
+  return sum;
+}
+
+export function addsNumbersDevidedBy3And2(arr: number[]): number {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (i % 2 === 0 && arr[i] % 3 === 0 && arr[i] % 2 === 0) sum += arr[i];
+  }
+  return sum;
+}
+
+export function addsOddNumberAndHigherThan15(arr: number[]): number {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0 && arr[i] > 15) sum += arr[i];
+  }
+  return sum;
+}
+
+export function addPerfectSquaresAndEvenNumbers(arr: number[]): number {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    const n = Math.floor(Math.sqrt(arr[i]));
+    if (arr[i] % 2 === 0 && n * n === arr[i]) {
+      sum += arr[i];
+    }
+  }
+  return sum;
+}
