be927bfe7f89567a2191241eb159141676e1ceb4
roxana-oprea <oprearoxanaadolfina@gmail.com>
2019-09-04 18:24:15 +0300

    new tests for product

--- __tests__/produse.spec.ts
+++ __tests__/produse.spec.ts
@@ -1,8 +1,46 @@
-import { productPositiveNumbers } from '../src/produse';
+import {
+  productPositiveNumbers,
+  productNegativeNumbers,
+  productOddNumbers,
+  productEvenNumbers,
+  productSquaresNumbers,
+  productNumbersWithEvenIndex,
+  productNumbersDividedBy3,
+} from '../src/produse';
 
 describe('Sume', () => {
   it('calculates the product of positive numbers', () => {
     const arr = Object.freeze([1, -4, 2, -1, 3, 3]) as number[];
     expect(productPositiveNumbers(arr)).toBe(18);
+  });
+
+  it('calculates the product of negative numbers', () => {
+    const arr = Object.freeze([-2, -1, 4, -3, 2]) as number[];
+    expect(productNegativeNumbers(arr)).toBe(-6);
+  });
+
+  it('calculates the product of odd numbers', () => {
+    const arr = Object.freeze([2, -3, 0, 4, 1, -2]) as number[];
+    expect(productOddNumbers(arr)).toBe(-16);
+  });
+
+  it('calculates the product of even numbers', () => {
+    const arr = Object.freeze([-1, 0, -3, 8, -7]) as number[];
+    expect(productEvenNumbers(arr)).toBe(-21);
+  });
+
+  it('calculates the product of perfect squares numbers', () => {
+    const arr = Object.freeze([4, 9, 19, 5, 6, -6]) as number[];
+    expect(productSquaresNumbers(arr)).toBe(36);
+  });
+
+  it('calculates the product of numbers with even index', () => {
+    const arr = Object.freeze([2, 1, -3, -2, 3, 4, 2]) as number[];
+    expect(productNumbersWithEvenIndex(arr)).toBe(-36);
+  });
+
+  it('calculate the product of numbers wich are divisible by 3', () => {
+    const arr = Object.freeze([3, 1, 7, 9]) as number[];
+    expect(productNumbersDividedBy3(arr)).toBe(27);
   });
 });
--- src/produse.ts
+++ src/produse.ts
@@ -5,3 +5,52 @@ export function productPositiveNumbers(arr: number[]): number {
   }
   return product;
 }
+
+export function productNegativeNumbers(arr: number[]): number {
+  let product = 1;
+  arr.map(item => {
+    if (item !== 0 && item < 0) product *= item;
+  });
+  return product;
+}
+
+export function productOddNumbers(arr: number[]): number {
+  let product = 1;
+  arr.map(item => {
+    if (item !== 0 && item % 2 === 0) product *= item;
+  });
+  return product;
+}
+
+export function productEvenNumbers(arr: number[]): number {
+  let product = 1;
+  arr.map(item => {
+    if (item !== 0 && item % 2 !== 0) product *= item;
+  });
+  return product;
+}
+
+export function productSquaresNumbers(arr: number[]): number {
+  let product = 1;
+  arr.map(item => {
+    const number = Math.floor(Math.sqrt(item));
+    if (item > 0 && number * number === item) product *= item;
+  });
+  return product;
+}
+
+export function productNumbersWithEvenIndex(arr: number[]): number {
+  let product = 1;
+  arr.map((item, index) => {
+    if (index % 2 === 0) product *= item;
+  });
+  return product;
+}
+
+export function productNumbersDividedBy3(arr: number[]): number {
+  let product = 1;
+  arr.map(item => {
+    if (item % 3 === 0) product *= item;
+  });
+  return product;
+}
