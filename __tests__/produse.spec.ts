c104c1cf644b169f4d72fd26d624f00d269c966d
roxana-oprea <oprearoxanaadolfina@gmail.com>
2019-09-05 20:47:42 +0300

    add latest tests for product

--- __tests__/produse.spec.ts
+++ __tests__/produse.spec.ts
@@ -6,6 +6,13 @@ import {
   productSquaresNumbers,
   productNumbersWithEvenIndex,
   productNumbersDividedBy3,
+  productNumbersDividedBy5,
+  productOfSquares,
+  productNumbersInInterval,
+  productOfPositiveNumbersAndDevisibleBy5,
+  productOfNumbersDevidedBy3And2,
+  prodyctOfOddNumberAndHigherThan15,
+  productOfPerfectSquaresAndEvenNumbers,
 } from '../src/produse';
 
 describe('Sume', () => {
@@ -42,5 +49,40 @@ describe('Sume', () => {
   it('calculate the product of numbers wich are divisible by 3', () => {
     const arr = Object.freeze([3, 1, 7, 9]) as number[];
     expect(productNumbersDividedBy3(arr)).toBe(27);
+  });
+
+  it('calculates the product of numbers divided by 5 the rest is 3', () => {
+    const arr = Object.freeze([13, 8, 5, 2]) as number[];
+    expect(productNumbersDividedBy5(arr)).toBe(104);
+  });
+
+  it('calculated the product of squares', () => {
+    const arr = Object.freeze([2, 3, 4]) as number[];
+    expect(productOfSquares(arr)).toBe(576);
+  });
+
+  it('calculates product between interval [-1, 9]', () => {
+    const arr = Object.freeze([2, 12, -1, 8, 16]) as number[];
+    expect(productNumbersInInterval(arr)).toBe(-16);
+  });
+
+  it('calculates the product of positive numbers and divisible by 5', () => {
+    const arr = Object.freeze([-2, 1, 5, -5, 10]) as number[];
+    expect(productOfPositiveNumbersAndDevisibleBy5(arr)).toBe(50);
+  });
+
+  it('calculates the product of numbers divided by 3 and 2 and are at even position', () => {
+    const arr = Object.freeze([2, 3, 6, 5, 12]) as number[];
+    expect(productOfNumbersDevidedBy3And2(arr)).toBe(72);
+  });
+
+  it('calculates the product of odd numbers and higher than 15', () => {
+    const arr = Object.freeze([2, 3, 17, 19, 14]) as number[];
+    expect(prodyctOfOddNumberAndHigherThan15(arr)).toBe(323);
+  });
+
+  it('calculates the product of perfect squares numbers and are even', () => {
+    const arr = Object.freeze([4, 9, 19, 5, 6]) as number[];
+    expect(productOfPerfectSquaresAndEvenNumbers(arr)).toBe(4);
   });
 });
--- src/produse.ts
+++ src/produse.ts
@@ -54,3 +54,60 @@ export function productNumbersDividedBy3(arr: number[]): number {
   });
   return product;
 }
+
+export function productNumbersDividedBy5(arr: number[]): number {
+  let product = 1;
+  arr.map(item => {
+    if (item % 5 === 3) product *= item;
+  });
+  return product;
+}
+
+export function productOfSquares(arr: number[]): number {
+  let product = 1;
+  arr.map(item => {
+    if (item !== 0) product *= item * item;
+  });
+  return product;
+}
+
+export function productNumbersInInterval(arr: number[]): number {
+  let product = 1;
+  arr.map(item => {
+    if (item >= -1 && item <= 9) product *= item;
+  });
+  return product;
+}
+
+export function productOfPositiveNumbersAndDevisibleBy5(arr: number[]): number {
+  let product = 1;
+  arr.map(item => {
+    if (item > 0 && item % 5 === 0) product *= item;
+  });
+  return product;
+}
+
+export function productOfNumbersDevidedBy3And2(arr: number[]): number {
+  let product = 1;
+  arr.map(item => {
+    if (item > 0 && item % 3 === 0 && item % 2 === 0) product *= item;
+  });
+  return product;
+}
+
+export function prodyctOfOddNumberAndHigherThan15(arr: number[]): number {
+  let product = 1;
+  arr.map(item => {
+    if (item > 0 && item > 15 && item % 2 !== 0) product *= item;
+  });
+  return product;
+}
+
+export function productOfPerfectSquaresAndEvenNumbers(arr: number[]): number {
+  let product = 1;
+  arr.map(item => {
+    const n = Math.floor(Math.sqrt(item));
+    if (item > 0 && item % 2 === 0 && n * n === item) product *= item;
+  });
+  return product;
+}
