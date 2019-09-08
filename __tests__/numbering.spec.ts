526eed4c2ccc109360d3764394d3b7050b484b79
roxana-oprea <oprearoxanaadolfina@gmail.com>
2019-09-08 17:30:35 +0300

    adds more tests for numbering

--- __tests__/numbering.spec.ts
+++ __tests__/numbering.spec.ts
@@ -1,13 +1,70 @@
-import { getPositiveNumbers, getNegativeNumbers } from '../src/numbering';
+import {
+  getPositiveNumbers,
+  getNegativeNumbers,
+  getEvenNumbers,
+  getOddNumbers,
+  getPerfectSquares,
+  getNumbersAtEvenPosition,
+  getElementsDivisibleBy3,
+  getElementsDivisibleBy5,
+  getElementsInInterval,
+  getElementsPositiveAndDivisibleBy5,
+  getElementsDevidedBy3And2,
+} from '../src/numbering';
 
 describe('Numbering', () => {
-  it('return the elements which are positive numbers', () => {
+  it('returns the elements which are positive numbers', () => {
     const arr = Object.freeze([1, -4, 2, -1, 3, -3]);
     expect(getPositiveNumbers(arr)).toEqual([1, 2, 3]);
   });
 
-  it('return the elements which are negative numbers', () => {
+  it('returns the elements which are negative numbers', () => {
     const arr = Object.freeze([3, 2, -1, 9, -8]);
     expect(getNegativeNumbers(arr)).toEqual([-1, -8]);
+  });
+
+  it('returns the elements which are even numbers', () => {
+    const arr = Object.freeze([3, 2, 7, 8, 4, 9]);
+    expect(getEvenNumbers(arr)).toEqual([2, 8, 4]);
+  });
+
+  it('returns the elements which are odd numbers', () => {
+    const arr = Object.freeze([1, 5, 2, 3, 8, 10]);
+    expect(getOddNumbers(arr)).toEqual([1, 5, 3]);
+  });
+
+  it('returns the elements which are perfect squares', () => {
+    const arr = Object.freeze([4, 9, 19, 5, 16, -6]);
+    expect(getPerfectSquares(arr)).toEqual([4, 9, 16]);
+  });
+
+  it('returns the elements which are at even position', () => {
+    const arr = Object.freeze([2, -1, 4, -5, -7]);
+    expect(getNumbersAtEvenPosition(arr)).toEqual([2, 4, -7]);
+  });
+
+  it('returns the elements which are divisible by 3', () => {
+    const arr = Object.freeze([2, 3, 1, -5, 9, 6]);
+    expect(getElementsDivisibleBy3(arr)).toEqual([3, 9, 6]);
+  });
+
+  it('returns the elements divided by 5 the rest is 3', () => {
+    const arr = Object.freeze([13, 8, 5, 2]);
+    expect(getElementsDivisibleBy5(arr)).toEqual([13, 8]);
+  });
+
+  it('returns the elements between interval [-1, 9]', () => {
+    const arr = Object.freeze([2, 1, 7, -2, 9, 15]);
+    expect(getElementsInInterval(arr)).toEqual([2, 1, 7, 9]);
+  });
+
+  it('return the elements which are positive and divisible with 5', () => {
+    const arr = Object.freeze([-2, 1, 5, -5, 10]);
+    expect(getElementsPositiveAndDivisibleBy5(arr)).toEqual([5, 10]);
+  });
+
+  it('returns the elements divided by 3 and 2 and are at even position', () => {
+    const arr = Object.freeze([2, 3, 6, 5, 12, 18]);
+    expect(getElementsDevidedBy3And2(arr)).toEqual([6, 12]);
   });
 });
--- src/numbering.ts
+++ src/numbering.ts
@@ -5,3 +5,42 @@ export function getPositiveNumbers(arr: readonly number[]): number[] {
 export function getNegativeNumbers(arr: readonly number[]): number[] {
   return arr.filter(elem => elem < 0);
 }
+
+export function getEvenNumbers(arr: readonly number[]): number[] {
+  return arr.filter(elem => elem % 2 === 0);
+}
+
+export function getOddNumbers(arr: readonly number[]): number[] {
+  return arr.filter(elem => elem % 2 !== 0);
+}
+
+export function getPerfectSquares(arr: readonly number[]): number[] {
+  return arr.filter(elem => {
+    const number = Math.floor(Math.sqrt(elem));
+    return number * number === elem;
+  });
+}
+
+export function getNumbersAtEvenPosition(arr: readonly number[]): number[] {
+  return arr.filter((elem, index) => index % 2 === 0);
+}
+
+export function getElementsDivisibleBy3(arr: readonly number[]): number[] {
+  return arr.filter(elem => elem % 3 === 0);
+}
+
+export function getElementsDivisibleBy5(arr: readonly number[]): number[] {
+  return arr.filter(elem => elem % 5 === 3);
+}
+
+export function getElementsInInterval(arr: readonly number[]): number[] {
+  return arr.filter(elem => elem >= -1 && elem <= 9);
+}
+
+export function getElementsPositiveAndDivisibleBy5(arr: readonly number[]): number[] {
+  return arr.filter(elem => elem > 0 && elem % 5 === 0);
+}
+
+export function getElementsDevidedBy3And2(arr: readonly number[]): number[] {
+  return arr.filter((elem, index) => index % 2 === 0 && elem % 2 === 0 && elem % 3 === 0);
+}
