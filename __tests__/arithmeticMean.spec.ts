9d965ef2da42ffd1d3a625cc70cad02be2d73377
roxana-oprea <oprearoxanaadolfina@gmail.com>
2019-09-14 21:18:16 +0300

    add last tests for arithmetic mean

--- __tests__/arithmeticMean.spec.ts
+++ __tests__/arithmeticMean.spec.ts
@@ -5,6 +5,12 @@ import {
   getAMOddNumbers,
   getAMPerfectSquaresNumbers,
   getAMofNumbersAtEvenPosition,
+  getAMofNumbersDivisibleBy3,
+  getAMofNumbersDividedBy5,
+  getAMofSquares,
+  getAMofNumbersInInterval,
+  getAMofPositiveNumbersAndDevidedBy5,
+  getAMofNumbersDevidedBy3And2,
 } from '../src/arithmeticMean';
 
 describe('Arithmetic Mean', () => {
@@ -36,5 +42,35 @@ describe('Arithmetic Mean', () => {
   it('calculates the aritmetic mean of numbers at even position', () => {
     const arr = Object.freeze([3, 1, 2, 4, 1]);
     expect(getAMofNumbersAtEvenPosition(arr)).toBe(2);
+  });
+
+  it('calculates the arithmetic mean of which are divisible by 3', () => {
+    const arr = Object.freeze([3, 1, 7, 9]);
+    expect(getAMofNumbersDivisibleBy3(arr)).toBe(6);
+  });
+
+  it('calculates the arithmetic mean of numbers divided by 5 the rest is 3', () => {
+    const arr = Object.freeze([13, 8, 5, 2]);
+    expect(getAMofNumbersDividedBy5(arr)).toBe(10.5);
+  });
+
+  it('calculates the arithmetic mean of squares', () => {
+    const arr = Object.freeze([3, 4]);
+    expect(getAMofSquares(arr)).toBe(12.5);
+  });
+
+  it('calculates the arithmetic mean of numbers between interval [-1, 9]', () => {
+    const arr = Object.freeze([2, 12, -1, 8, 16]);
+    expect(getAMofNumbersInInterval(arr)).toBe(3);
+  });
+
+  it('calculates the arithmetic mean of positive numbers and divided by 5', () => {
+    const arr = Object.freeze([-2, 1, 5, -5, 10]);
+    expect(getAMofPositiveNumbersAndDevidedBy5(arr)).toBe(7.5);
+  });
+
+  it('calculates the arithmetic mean of numbers which are on even position and are divisible by 3 and 2', () => {
+    const arr = Object.freeze([2, 3, 6, 5, 12]);
+    expect(getAMofNumbersDevidedBy3And2(arr)).toBe(9);
   });
 });
--- src/arithmeticMean.ts
+++ src/arithmeticMean.ts
@@ -1,3 +1,5 @@
+import { arrayExpression } from '@babel/types';
+
 export function getAMPositiveNumbers(arr: readonly number[]): number {
   const newArr = arr.filter(elem => elem > 0);
   const total = newArr.reduce((acc, elem) => acc + elem, 0);
@@ -34,5 +36,41 @@ export function getAMPerfectSquaresNumbers(arr: readonly number[]): number {
 export function getAMofNumbersAtEvenPosition(arr: readonly number[]): number {
   const newArr = arr.filter((elem, index) => index % 2 === 0);
   const total = newArr.reduce((acc, elem) => acc + elem, 0);
+  return total / newArr.length;
+}
+
+export function getAMofNumbersDivisibleBy3(arr: readonly number[]): number {
+  const newArr = arr.filter(elem => elem % 3 === 0);
+  const total = newArr.reduce((acc, elem) => acc + elem);
+  return total / newArr.length;
+}
+
+export function getAMofNumbersDividedBy5(arr: readonly number[]): number {
+  const newArr = arr.filter(elem => elem % 5 === 3);
+  const total = newArr.reduce((acc, elem) => acc + elem);
+  return total / newArr.length;
+}
+
+export function getAMofSquares(arr: readonly number[]): number {
+  const newArr = arr.map(elem => elem * elem);
+  const total = newArr.reduce((acc, elem) => acc + elem);
+  return total / newArr.length;
+}
+
+export function getAMofNumbersInInterval(arr: readonly number[]): number {
+  const newArr = arr.filter(elem => elem >= -1 && elem <= 9);
+  const total = newArr.reduce((acc, elem) => acc + elem);
+  return total / newArr.length;
+}
+
+export function getAMofPositiveNumbersAndDevidedBy5(arr: readonly number[]): number {
+  const newArr = arr.filter(elem => elem > 0 && elem % 5 === 0);
+  const total = newArr.reduce((acc, elem) => acc + elem);
+  return total / newArr.length;
+}
+
+export function getAMofNumbersDevidedBy3And2(arr: readonly number[]): number {
+  const newArr = arr.filter((elem, index) => index % 2 === 0 && elem % 6 === 0);
+  const total = newArr.reduce((acc, elem) => acc + elem);
   return total / newArr.length;
 }
