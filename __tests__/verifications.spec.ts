70993099aec7e4d2185f775444f772398fc0cd14
roxana-oprea <oprearoxanaadolfina@gmail.com>
2019-09-23 18:50:07 +0300

    add tests with some method

--- __tests__/verifications.spec.ts
+++ __tests__/verifications.spec.ts
@@ -1,4 +1,12 @@
-import { checkAllEvenNumbers, checkAllOddNumbers } from '../src/verifications';
+import {
+  checkAllEvenNumbers,
+  checkAllOddNumbers,
+  checkNumbersDivisibleBy4,
+  checkOddNumbersAndBiggerThan15,
+  hasAtLeastOneEvenElement,
+  hasAtLeastOneOddElement,
+  hasAtLeastOneElementDivisibleBy4,
+} from '../src/verifications';
 
 describe('Verifications', () => {
   it('checks if all the elements are even', () => {
@@ -9,5 +17,30 @@ describe('Verifications', () => {
   it('checks if all the elements are odd', () => {
     const arr = Object.freeze([3, 1, 5, 9]);
     expect(checkAllOddNumbers(arr)).toBe(true);
+  });
+
+  it('checks if all the elements are divisible with 4', () => {
+    const arr = Object.freeze([16, 36, 4]);
+    expect(checkNumbersDivisibleBy4(arr)).toBe(true);
+  });
+
+  it('checks if all elements are odd and bigger than 15', () => {
+    const arr = Object.freeze([4, 23, 19, 33, 45]);
+    expect(checkOddNumbersAndBiggerThan15(arr)).toBe(false);
+  });
+
+  it('checks if the array has at least one even element', () => {
+    const arr = Object.freeze([5, 9, 15, 2, 4]);
+    expect(hasAtLeastOneEvenElement(arr)).toBe(true);
+  });
+
+  it('checks if the array has at least one odd element', () => {
+    const arr = Object.freeze([8, 6, 4, 2]);
+    expect(hasAtLeastOneOddElement(arr)).toBe(false);
+  });
+
+  it('checks if the array has at least one element divisible by 4', () => {
+    const arr = Object.freeze([1, 2, 3, 5]);
+    expect(hasAtLeastOneElementDivisibleBy4(arr)).toBe(false);
   });
 });
--- src/verifications.ts
+++ src/verifications.ts
@@ -11,3 +11,32 @@ export function checkAllOddNumbers(arr: readonly number[]): boolean {
   }
   return true;
 }
+
+export function checkNumbersDivisibleBy4(arr: readonly number[]): boolean {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 4 !== 0) return false;
+  }
+  return true;
+}
+
+export function checkOddNumbersAndBiggerThan15(arr: readonly number[]): boolean {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0 && arr[i] < 15) return false;
+  }
+  return true;
+}
+
+export function hasAtLeastOneEvenElement(arr: readonly number[]): boolean {
+  return arr.some(elem => elem % 2 === 0);
+}
+
+export function hasAtLeastOneOddElement(arr: readonly number[]): boolean {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) return false;
+  }
+  return true;
+}
+
+export function hasAtLeastOneElementDivisibleBy4(arr: readonly number[]): boolean {
+  return arr.some(elem => elem % 4 === 0);
+}
