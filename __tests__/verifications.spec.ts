bd1405710dc782a5b0e588a60776fad16f8f6a93
roxana-oprea <oprearoxanaadolfina@gmail.com>
2019-09-27 11:36:18 +0300

    add last test for verifications

--- __tests__/verifications.spec.ts
+++ __tests__/verifications.spec.ts
@@ -8,6 +8,15 @@ import {
   hasAtLeastOneElementDivisibleBy4,
   hasAtLeastOneOddNumberAndBiggerThan15,
   checkArrayForAnyEvenNumber,
+  checkArrayForAnyOddNumber,
+  checkArrayForAnyNumberDivisibleBy4,
+  checkArrayForAnyOddNumberAndBiggerThan15,
+  ascendingSort,
+  descendingSort,
+  constantArray,
+  equalLengthOfArrays,
+  checksFirst3Elements,
+  checksFirstAndLast3Elements,
 } from '../src/verifications';
 
 describe('Verifications', () => {
@@ -54,5 +63,53 @@ describe('Verifications', () => {
   it('checks if the array does not contain any even element', () => {
     const arr = Object.freeze([1, 9, 3, 5]);
     expect(checkArrayForAnyEvenNumber(arr)).toBe(true);
+  });
+
+  it('checks if the array does not contain any odd element', () => {
+    const arr = Object.freeze([1, 9, 3, 5]);
+    expect(checkArrayForAnyOddNumber(arr)).toBe(false);
+  });
+
+  it('checks if the array does not contain elements divisible by 4', () => {
+    const arr = Object.freeze([16, 19, 33, 5]);
+    expect(checkArrayForAnyNumberDivisibleBy4(arr)).toBe(false);
+  });
+
+  it('checks if the array does not contain odd elements and bigger than 15', () => {
+    const arr = Object.freeze([16, 9, 23, 5, 17]);
+    expect(checkArrayForAnyOddNumberAndBiggerThan15(arr)).toBe(false);
+  });
+
+  it('sorts the elements of an array ascending', () => {
+    const arr = Object.freeze([16, 9, 23, 5, 17]);
+    expect(ascendingSort(arr)).toEqual([5, 9, 16, 17, 23]);
+  });
+
+  it('sorts the elements of an array descending', () => {
+    const arr = Object.freeze([16, 9, 23, 5, 17]);
+    expect(descendingSort(arr)).toEqual([23, 17, 16, 9, 5]);
+  });
+
+  it('checks for a constant array', () => {
+    const arr = Object.freeze([16, 16, 16]);
+    expect(constantArray(arr)).toBe(true);
+  });
+
+  it('checks for the equal length of 2 arrays', () => {
+    const arr1 = Object.freeze([1, 6, 5]);
+    const arr2 = Object.freeze([2, 4, 9]);
+    expect(equalLengthOfArrays(arr1, arr2)).toBe(true);
+  });
+
+  it('cheks if first array has first 3 elements equals with firts 3 elements from second array', () => {
+    const arr1 = Object.freeze([1, 2, 3, 6, 5]);
+    const arr2 = Object.freeze([1, 2, 3, 4, 9]);
+    expect(checksFirst3Elements(arr1, arr2)).toBe(true);
+  });
+
+  it('cheks if first array has first 3 elements equals with last 3 elements from second array', () => {
+    const arr1 = Object.freeze([1, 2, 3, 6, 5]);
+    const arr2 = Object.freeze([4, 9, 3, 2, 1]);
+    expect(checksFirstAndLast3Elements(arr1, arr2)).toBe(true);
   });
 });
--- src/verifications.ts
+++ src/verifications.ts
@@ -54,3 +54,70 @@ export function checkArrayForAnyEvenNumber(arr: readonly number[]): boolean {
   }
   return false;
 }
+
+export function checkArrayForAnyOddNumber(arr: readonly number[]): boolean {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) return true;
+  }
+  return false;
+}
+
+export function checkArrayForAnyNumberDivisibleBy4(arr: readonly number[]): boolean {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 4 === 0) return false;
+  }
+  return true;
+}
+
+export function checkArrayForAnyOddNumberAndBiggerThan15(arr: readonly number[]): boolean {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0 && arr[i] > 15) return false;
+  }
+  return true;
+}
+
+export function ascendingSort(arr: readonly number[]): number[] {
+  const cloneArr = [...arr];
+  const sortedArray = cloneArr.sort((a: number, b: number) => a - b);
+  return sortedArray;
+}
+
+export function descendingSort(arr: readonly number[]): number[] {
+  const cloneArr = [...arr];
+  const sortedArray = cloneArr.sort((a: number, b: number) => b - a);
+  return sortedArray;
+}
+
+export function constantArray(arr: readonly number[]): boolean {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) return true;
+  }
+  return false;
+}
+
+export function equalLengthOfArrays(arr1: readonly number[], arr2: readonly number[]): boolean {
+  if (arr1.length === arr2.length) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export function checksFirst3Elements(arr1: readonly number[], arr2: readonly number[]): boolean {
+  for (let i = 0; i < 3; i++) {
+    if (arr1[i] === arr2[i]) return true;
+  }
+  return false;
+}
+
+export function checksFirstAndLast3Elements(
+  arr1: readonly number[],
+  arr2: readonly number[],
+): boolean {
+  for (let i = 0; i < 3; i++) {
+    for (let j = arr2.length - 1; j >= arr2.length - 3; j--) {
+      if (arr1[i] === arr2[j]) return true;
+    }
+  }
+  return false;
+}
