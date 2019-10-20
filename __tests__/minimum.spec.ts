7dee94f610b5fbbfd3b569c6a8a2abc0d54139a8
roxana-oprea <oprearoxanaadolfina@gmail.com>
2019-10-20 11:04:02 +0300

    add tests for exercices with find the minimum

--- __tests__/minimum.spec.ts
+++ __tests__/minimum.spec.ts
@@ -4,6 +4,10 @@ import {
   getTheMinimumFromEvenNumbers,
   getTheMinimumFromOddNumbers,
   getTheMinimumFromPerfectSquaresNumbers,
+  getTheMinFromNumbersOnEvenPosition,
+  getTheMinFromNumbersDivisibleBy3,
+  getTheMinFromNumbersDividedBy5,
+  getTheMinOfSquares,
 } from '../src/minimum';
 
 describe('Minimum', () => {
@@ -30,5 +34,25 @@ describe('Minimum', () => {
   it('returns the minimum from the perfect squares numbers', () => {
     const arr = Object.freeze([4, 9, 19, 5, 6, -6]);
     expect(getTheMinimumFromPerfectSquaresNumbers(arr)).toBe(4);
+  });
+
+  it('returns the minimum from numbers which are on even position', () => {
+    const arr = Object.freeze([3, 1, 6, 9, 5, 2, 8]);
+    expect(getTheMinFromNumbersOnEvenPosition(arr)).toBe(5);
+  });
+
+  it('returns the minimum from numbers which are divisible by 3', () => {
+    const arr = Object.freeze([3, 1, 7, 9]);
+    expect(getTheMinFromNumbersDivisibleBy3(arr)).toBe(3);
+  });
+
+  it('returns the minimum from numbers which divided by 5 the rest is 3', () => {
+    const arr = Object.freeze([13, 8, 5, 2, 10]);
+    expect(getTheMinFromNumbersDividedBy5(arr)).toBe(8);
+  });
+
+  it('returns the minimum of squares', () => {
+    const arr = Object.freeze([2, 9, 5, 7, 4, 81]);
+    expect(getTheMinOfSquares(arr)).toBe(4);
   });
 });
--- src/minimum.ts
+++ src/minimum.ts
@@ -1,5 +1,3 @@
-import { numberLiteralTypeAnnotation } from '@babel/types';
-
 export function getTheMinimumFromPozitiveNumbers(arr: readonly number[]): number {
   let min: number | undefined;
   for (let i = 0; i < arr.length; i++) {
@@ -57,6 +55,63 @@ export function getTheMinimumFromOddNumbers(arr: readonly number[]): number {
 }
 
 export function getTheMinimumFromPerfectSquaresNumbers(arr: readonly number[]): number {
+  let min: number | undefined;
+  for (let i = 0; i < arr.length; i++) {
+    const num = Math.floor(Math.sqrt(arr[i]));
+    if (num * num === arr[i]) {
+      if (min === undefined) {
+        min = arr[i];
+      } else if (arr[i] < min) {
+        min = arr[i];
+      }
+    }
+  }
+  return min as number;
+}
+
+export function getTheMinFromNumbersOnEvenPosition(arr: readonly number[]): number {
+  let min: number | undefined;
+  for (let i = 2; i < arr.length; i++) {
+    if (i % 2 === 0) {
+      if (min === undefined) {
+        min = arr[i];
+      } else if (arr[i] < min) {
+        min = arr[i];
+      }
+    }
+  }
+  return min as number;
+}
+
+export function getTheMinFromNumbersDivisibleBy3(arr: readonly number[]): number {
+  let min: number | undefined;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 3 === 0) {
+      if (min === undefined) {
+        min = arr[i];
+      } else if (arr[i] < min) {
+        min = arr[i];
+      }
+    }
+  }
+  return min as number;
+}
+
+export function getTheMinFromNumbersDividedBy5(arr: readonly number[]): number {
+  let min: number | undefined;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 5 === 3) {
+      if (min === undefined) {
+        min = arr[i];
+      } else if (arr[i] < min) {
+        min = arr[i];
+      }
+    }
+  }
+  return min as number;
+}
+
+export function getTheMinOfSquares(arr: readonly number[]): number {
   let min: number | undefined;
   for (let i = 0; i < arr.length; i++) {
     const num = Math.floor(Math.sqrt(arr[i]));
